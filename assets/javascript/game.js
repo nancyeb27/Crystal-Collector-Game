8a51ee3af00814dfb453048c6e4470417874499e
Nancyb <nancyeb27@yahoo.com>
2018-10-07 14:54:02 -0600

    Added Folders

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+var compNum;
+var wins;
+var loss;
+
+
+for (var i = 0; i < 4; i++) {
+    var crystal = $("<div>");
+        crystal.attr("class", 'crystal');
+
+        $(".crystals").append(crystal);
+            
+
+    
+}
+
+
+
+
+
+// Psuosto code
+
+// a game with 4 crystals and a random computer generated number.
+// 
+//Every crystal needs to have a random number between 1 and 12
+// Computer generated number will be between 19 and 120
+//A new random number should be generated for each crystal 
+//and computer total after every win and loss
+
+//Each time a crystal is clicked - its value is added to the total
+//UNITL the total equals to or is greater then the computers number.
+
+
+// if total of crystal value is less then computer number - continue pick another crystal
+// if total of crystal value is equal to the computer number -Win
+// if total of  crystal value is greater than the computer number - Loss
+
+
+
